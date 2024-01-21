dea8959272bbcba380d64681fef95e1bffec8f2a
krzemLech <krzemmo@gmail.com>
2024-01-21 16:07:10 +0100

    debugging 4

--- netlify/functions/send-msg.mjs
+++ netlify/functions/send-msg.mjs
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { getXataClient } from "./@xata.io/netlify";
+import { getXataClient } from "./@xata.io";
 
 export default async (req, context) => {
   const email = Netlify.env.get("EMAIL");
@@ -17,7 +17,7 @@ export default async (req, context) => {
   const db_client = getXataClient();
   const db_data = await db_client["portfolio-db"].messages.getAll();
 
-  return new Response(JSON.stringify({ msg: "xata connected 3", db_data }));
+  return new Response(JSON.stringify({ msg: "xata connected 4", db_data }));
 
   // const canSend = await fetch(checkUrl).then((res) => res.json(), {
   //   headers: { "x-app-id": appId },
